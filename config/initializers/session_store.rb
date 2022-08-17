c28b11382edba1cc0e570cacf8d3e4458b02b6d5
Zicrou <aazseck@gmail.com>
2022-08-17 14:19:53 -0400

    done

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,5 @@
-Rails.application.config.session_store :cookie_store, key: "_authentification_app", domain: "zicrou-rails-react.herokuapp.com"
+if Rails.env == "production"
+    Rails.application.config.session_store :cookie_store, key: "_authentification_app", domain: "zicrou-rails-react.herokuapp.com"
+else 
+    Rails.application.config.session_store :cookie_store, key: "_authentification_app"
+end
